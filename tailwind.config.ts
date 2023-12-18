2a248bd7dd6d2c4dc13332f9d6320ae54dfb737d
boyzwhocried <verrel.alsyoumi@gmail.com>
2023-12-18 23:59:17 +0700

    chore(tailwind): Update tailwind.config.ts
    Enhance tailwind.config.ts with additional styles and configurations. The changes include:
    
    - Added new animations: "spin-slow" and "spin-extra-slow".
    - Expanded color definitions for text, background, primary, secondary, and accent.
    - Improved background image definitions.
    
    Also, updated dependencies with the addition of react-icons.
    
    This commit addresses styling enhancements and improves the overall configuration in tailwind.config.ts.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        "spin-extra-slow": "spin 5s linear infinite",
+      },
+      colors: {
+        text: "hsl(var(--text) / <alpha-value>)",
+        background: "hsl(var(--background) / <alpha-value>)",
+        primary: "hsl(var(--primary) / <alpha-value>)",
+        secondary: "hsl(var(--secondary) / <alpha-value>)",
+        accent: "hsl(var(--accent) / <alpha-value>)",
+      },
     },
   },
   plugins: [],
