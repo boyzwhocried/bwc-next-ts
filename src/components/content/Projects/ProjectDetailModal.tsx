f57f8a428c27da312ceed758ba9e1d620c24ccd9
boyzwhocried <verrel.alsyoumi@gmail.com>
2023-12-26 00:51:12 +0700

    refactor(ProjectDetailModal): Simplify image handling with React Carousel
    - Remove individual image navigation buttons and implement a responsive image carousel using the 'react-responsive-carousel' library.
    - Eliminate unnecessary state and functions for manual image navigation.
    - Improve the overall structure and readability of the component.
    
    Closes #23

--- src/components/content/Projects/ProjectDetailModal.tsx
+++ src/components/content/Projects/ProjectDetailModal.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
-import React, { useRef, useEffect, useState } from "react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
+import Image from "next/image";
+import React, { useRef, useEffect } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 interface ProjectDetailModalProps {
   project: {
     projectName: string;
@@ -11,26 +12,11 @@ interface ProjectDetailModalProps {
   };
   onClose: () => void;
 }
-
 const ProjectDetailModal: React.FC<ProjectDetailModalProps> = ({
   project,
   onClose,
 }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const modalRef = useRef<HTMLDivElement>(null);
-
-  const handleNextImage = () => {
-    setCurrentImageIndex(
-      (prevIndex) => (prevIndex + 1) % project.projectImages.length
-    );
-  };
-
-  const handlePrevImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? project.projectImages.length - 1 : prevIndex - 1
-    );
-  };
-
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
@@ -42,18 +28,16 @@ const ProjectDetailModal: React.FC<ProjectDetailModalProps> = ({
     };
     // Attach event listener when the component mounts
     document.addEventListener("click", handleClickOutside);
-
     // Remove event listener when the component unmounts
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
-  }, [onClose]); // Empty dependency array ensures the effect runs only once on mount
-
+  }, [onClose]);
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-background bg-opacity-50 overflow-y-auto z-50">
       <div
         ref={modalRef}
-        className="relative max-w-3xl w-full m-8 p-8 bg-background rounded-lg shadow-md max-h-[90vh] overflow-y-scroll"
+        className="relative max-w-3xl w-full m-8 py-8 bg-background rounded-lg shadow-md max-h-[90vh] overflow-y-scroll"
       >
         <button
           className="absolute top-4 right-4 text-text"
@@ -75,35 +59,26 @@ const ProjectDetailModal: React.FC<ProjectDetailModalProps> = ({
             />
           </svg>
         </button>
-        <h2 className="text-3xl font-bold mb-4">{project.projectName}</h2>
-        <div className="relative aspect-w-16 aspect-h-9 mb-4">
-          <Image
-            className="object-cover object-center rounded aspect-video"
-            src={project.projectImages[currentImageIndex]}
-            alt={`${project.projectName} Image ${currentImageIndex + 1}`}
-            width={1000}
-            height={1000}
-          />
-          <button
-            className="absolute top-1/2 transform -translate-y-1/2 left-4 text-text bg-primary rounded-full p-2 opacity-50"
-            onClick={handlePrevImage}
-            aria-label="go previous"
-          >
-            <FaArrowLeft />
-          </button>
-          <button
-            className="absolute top-1/2 transform -translate-y-1/2 right-4 text-text bg-primary rounded-full p-2 opacity-50"
-            onClick={handleNextImage}
-            aria-label="go next"
-          >
-            <FaArrowRight />
-          </button>
-          <p className="text-sm text-center">
-            Image {currentImageIndex + 1} of {project.projectImages.length}
-          </p>
-        </div>
-        <p className="text-base mb-4">{project.projectDescription}</p>
-        <div className="mb-4">
+        <h2 className="text-3xl font-bold mb-4 px-8">{project.projectName}</h2>
+        <Carousel
+          className="relative aspect-w-16 aspect-h-9 mb-4"
+          infiniteLoop
+          ariaLabel={project.projectName}
+        >
+          {project.projectImages.map((projectImg, index) => (
+            <div key={index}>
+              <Image
+                className="object-cover object-center aspect-video"
+                src={projectImg}
+                alt={`${project.projectName} Image ${index + 1}`}
+                width={1000}
+                height={1000}
+              />
+            </div>
+          ))}
+        </Carousel>
+        <p className="text-base mb-4 px-8">{project.projectDescription}</p>
+        <div className="mb-4 px-8">
           <h3 className="text-lg font-semibold">Stacks</h3>
           <div className="flex flex-wrap gap-2">
             {project.stack.map((tech, techIndex) => (
@@ -120,5 +95,4 @@ const ProjectDetailModal: React.FC<ProjectDetailModalProps> = ({
     </div>
   );
 };
-
 export default ProjectDetailModal;
