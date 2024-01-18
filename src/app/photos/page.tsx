7f0a7b5580fc8aaf773989ab1d7c4a3eef1947c8
boyzwhocried <verrel.alsyoumi@gmail.com>
2024-01-18 17:05:52 +0700

    feat(photos): Add initial setup for the "photos" page
    This commit introduces the initial setup for a new page called "photos" to showcase
    photography. Included are the necessary files and assets. Please note that the
    page is still a work in progress and requires further development.

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -2,6 +2,15 @@ import Image from "next/image";
 
 import photo1 from "/public/assets/image/photos/000044.jpg";
 import photo2 from "/public/assets/image/photos/000165220036.jpg";
+import photo3 from "/public/assets/image/photos/000015990015.jpg";
+import photo4 from "/public/assets/image/photos/000015950036.jpg";
+
+const photos = [photo1, photo2, photo3, photo4];
+
+const getRandomPosition = () => ({
+  top: `${Math.random() * 50}vh`, // Adjust the range based on your needs
+  left: `${Math.random() * 50}vw`,
+});
 
 export default function Page() {
   return (
@@ -10,13 +19,29 @@ export default function Page() {
         <div className="flex aspect-auto overflow-hidden">
           <Image
             className="flex object-cover h-screen w-screen"
-            src={photo1}
+            src={photo4}
             alt={""}
-            width={1000}
-            height={1000}
+            sizes="100vw"
+            width={0}
+            height={0}
           />
         </div>
-        <div className="flex aspect-auto overflow-hidden ">
+        {photos.map((photo, index) => (
+          <div
+            key={index}
+            className="flex aspect-auto overflow-hidden"
+            style={getRandomPosition()}
+          >
+            <Image
+              className="flex object-scale-down hover:scale-105 transition duration-500"
+              src={photo}
+              alt={""}
+              width={1000}
+              height={1000}
+            />
+          </div>
+        ))}
+        {/* <div className="flex aspect-auto overflow-hidden ">
           <Image
             className="flex object-scale-down hover:scale-105 transition duration-500 "
             src={photo2}
@@ -34,6 +59,33 @@ export default function Page() {
             height={1000}
           />
         </div>
+        <div className="flex aspect-auto overflow-hidden ">
+          <Image
+            className="flex object-scale-down hover:scale-105 transition duration-500 "
+            src={photo3}
+            alt={""}
+            width={1000}
+            height={1000}
+          />
+        </div>
+        <div className="flex aspect-auto overflow-hidden ">
+          <Image
+            className="flex object-scale-down hover:scale-105 transition duration-500 "
+            src={photo1}
+            alt={""}
+            width={1000}
+            height={1000}
+          />
+        </div>
+        <div className="flex aspect-auto overflow-hidden ">
+          <Image
+            className="flex object-scale-down hover:scale-105 transition duration-500 "
+            src={photo3}
+            alt={""}
+            width={1000}
+            height={1000}
+          />
+        </div> */}
       </div>
     </>
   );
