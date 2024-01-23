d420802e6efe045bc7dead361390bd504596617e
boyzwhocried <verrel.alsyoumi@gmail.com>
2024-01-23 23:57:51 +0700

    feat(photos): Add initial setup for the "photos" page
    This commit introduces the initial setup for a new page called "photos" to showcase
    photography. Included are the necessary files and assets. Please note that the
    page is still a work in progress and requires further development.

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { CgMenu, CgClose } from "react-icons/cg";
 import UnderlineLink from "../others/UnderlineLink";
+import Link from "next/link";
 
 const navigation = [
   {
@@ -10,8 +11,8 @@ const navigation = [
     link: "/",
   },
   {
-    navigationName: "photos",
-    link: "/photos",
+    navigationName: "photograph",
+    link: "/photograph",
   },
 ];
 
@@ -25,15 +26,15 @@ function NavBar() {
   return (
     <nav
       className={`fixed top-0 w-full backdrop-blur-sm z-20 ${
-        isNavOpen ? "bg-background/75" : ""
+        isNavOpen && "bg-background/75"
       }`}
     >
       <div className="max-w-screen-2xl mx-auto p-4 flex items-center justify-between">
-        <a href="/" className="flex items-center">
+        <Link href="/" className="flex items-center">
           <span className="text-4xl sm:text-5xl font-bold bg-clip-text bg-gradient-to-r from-primary to-accent text-transparent">
             boyzwhocried
           </span>
-        </a>
+        </Link>
         <div className="hidden sm:flex space-x-6">
           {navigation.map((nav, index) => (
             <UnderlineLink
@@ -54,7 +55,10 @@ function NavBar() {
         </div>
       </div>
       {isNavOpen && (
-        <div className="sm:hidden p-4 flex flex-col items-end gap-3">
+        <div
+          className="sm:hidden p-4 flex flex-col items-end gap-3"
+          onClick={toggleNav}
+        >
           {navigation.map((nav, index) => (
             <UnderlineLink
               key={index}
