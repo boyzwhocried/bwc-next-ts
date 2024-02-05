ff0893cd122fed46acdc0ef2482487869fa8cef0
boyzwhocried <verrel.alsyoumi@gmail.com>
2024-02-06 01:19:07 +0700

    feat(navbar): Add 'login' navigation
    Introduce a new 'login' link in the navbar for easy access to the login page.

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -14,6 +14,10 @@ const navigation = [
     navigationName: "photograph",
     link: "/photograph",
   },
+  {
+    navigationName: "login",
+    link: "/auth/login",
+  },
 ];
 
 function NavBar() {
