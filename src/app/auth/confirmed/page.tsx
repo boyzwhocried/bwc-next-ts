f898fc21ab0426f9c7c16d440c1c94439a9f4aad
boyzwhocried <verrel.alsyoumi@gmail.com>
2024-02-10 01:08:42 +0700

    feat(auth): Update callback to redirect users to specified URL
    Previously, the authentication callback did not redirect users to a specific URL after successful authentication. This commit implements the functionality to redirect users to the URL they intended to visit after authentication.
    
    Changes made:
    - Modified the callback function to extract the intended redirect URL from the authentication request.
    - Implemented logic to redirect users to the specified URL upon successful authentication.
    
    This resolves the issue of users being redirected to a default page after authentication and enhances the user experience by allowing them to seamlessly navigate to their intended destination.

---
+++ src/app/auth/confirmed/page.tsx
@@ -0,0 +1,7 @@
+export default function Confirmed() {
+  return (
+    <div className="flex justify-center items-center h-screen w-full">
+      <p>Your account has been confirmed. You may close this page.</p>
+    </div>
+  );
+}
---
+++ src/app/auth/unauthenticated/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+export default function Unauthenticated() {
+  const loginUrl = "/auth/login";
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleLogin = () => {
+
+    const redirect = searchParams.get("next");
+    const absoluteURL = new URL(loginUrl, location.origin);
+    redirect
+      ? absoluteURL.searchParams.append("next", redirect as string)
+      : null;
+    router.replace(absoluteURL.toString());
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen w-full">
+      <p>
+        This page is protected. Please click{" "}
+        <button
+          className="text-accent underline hover:italic"
+          onClick={handleLogin}
+        >
+          here
+        </button>{" "}
+        to login and access it.
+      </p>
+    </div>
+  );
+}
